b2609b34a78a2fbc76b95fe555d1a0d85c8e1b95
upshorts <147702378+upshorts@users.noreply.github.com>
2023-10-12 14:06:39 +0530

    added index.html

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const ballLink = document.getElementById('ball-link');
-
-    ballLink.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent the default link behavior
-        const pageURL = ballLink.getAttribute('href');
-        window.location.href = pageURL; // Redirect to the specified URL
-    });
+document.getElementById("showOption").addEventListener("click", function() {
+    const optionContainer = document.getElementById("option-container");
+    optionContainer.classList.toggle("hidden");
 });
