4f21ce302ea05e7039574ba10c7419ac007c7923
upshorts <147702378+upshorts@users.noreply.github.com>
2023-10-14 21:54:55 +0530

    Delete save_message.php

--- save_message.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (isset($_POST['message']) && $_POST['message'] !== '') {
-        $message = $_POST['message'] . "\n";
-        file_put_contents('messages.txt', $message, FILE_APPEND | LOCK_EX);
-    }
-    header("Location: index.php");
-    exit;
-?>
