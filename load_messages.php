5b0a4ff4141d0a8b7e5e27b4bbb8370ca2aad1c5
upshorts <147702378+upshorts@users.noreply.github.com>
2023-10-14 21:54:46 +0530

    Delete load_messages.php

--- load_messages.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $messages = file('messages.txt', FILE_IGNORE_NEW_LINES);
-    foreach ($messages as $message) {
-        echo '<div class="message">' . htmlspecialchars($message) . '</div>';
-    }
-?>
